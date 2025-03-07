742a6b42350a1275825af7c4715eebcf4a28fe0c
zhangdong <zhangdong@selectdb.com>
2025-03-07 10:56:51 +0800

    1

---
+++ regression-test/suites/mtmv_p0/single_thread_cancel_mtmv_fail_test.groovy
@@ -0,0 +1,170 @@
+suite("single_thread_cancel_mtmv_fail_test") {
+
+    def waitingMTMVTaskFinishedCur = { def jobName ->
+        Thread.sleep(2000)
+        String showTasks = "select TaskId,JobId,JobName,MvId,Status from tasks('type'='mv') where JobName = '${jobName}' order by CreateTime ASC"
+        String status = "NULL"
+        List<List<Object>> result
+        long startTime = System.currentTimeMillis()
+        long timeoutTimestamp = startTime + 30 * 60 * 1000 // 5 min
+        do {
+            result = sql(showTasks)
+            logger.info("result: " + result.toString())
+            if (!result.isEmpty()) {
+                status = result.last().get(4)
+            }
+            logger.info("The state of ${showTasks} is ${status}")
+            sleep(10 * 1000)
+        } while (timeoutTimestamp > System.currentTimeMillis() && (status == 'PENDING' || status == 'RUNNING' || status == 'NULL'))
+        if (status != "SUCCESS") {
+            logger.info("status is not success")
+            return false
+        }
+        assertEquals("SUCCESS", status)
+        return true
+    }
+
+    def src_database_name = context.config.getDbNameByFile(context.file)
+    def table_name1 = "lineitem"
+    def table_name2 = "orders"
+
+sql """drop database if exists ${src_database_name};"""
+           sql """create database ${src_database_name};"""
+           sql """use ${src_database_name};"""
+
+           sql """drop table if exists ${table_name1}"""
+           sql """drop table if exists ${table_name2}"""
+           sql """CREATE TABLE lineitem (
+               l_orderkey    bigint NOT NULL,
+               l_linenumber  int not null,
+               l_partkey     int NOT NULL,
+               l_suppkey     int not null,
+               l_quantity    decimal(15, 2) NOT NULL,
+               l_extendedprice  decimal(15, 2) NOT NULL,
+               l_discount    decimal(15, 2) NOT NULL,
+               l_tax         decimal(15, 2) NOT NULL,
+               l_returnflag  VARCHAR(1) NOT NULL,
+               l_linestatus  VARCHAR(1) NOT NULL,
+               l_shipdate    DATE NOT NULL,
+               l_commitdate  DATE NOT NULL,
+               l_receiptdate DATE NOT NULL,
+               l_shipinstruct VARCHAR(25) NOT NULL,
+               l_shipmode     VARCHAR(10) NOT NULL,
+               l_comment      VARCHAR(44) NOT NULL,
+               l_null VARCHAR(1) NULL
+           )ENGINE=OLAP
+           UNIQUE KEY(`l_orderkey`)
+           COMMENT "OLAP"
+           DISTRIBUTED BY HASH(`l_orderkey`) BUCKETS 96
+           PROPERTIES (
+               "replication_num" = "1",
+               "colocate_with" = "lineitem_orders",
+               "enable_unique_key_merge_on_write" = "true"
+           );"""
+           sql """CREATE TABLE orders  (
+               o_orderkey       bigint NOT NULL,
+               o_custkey        int NOT NULL,
+               o_orderstatus    VARCHAR(1) NOT NULL,
+               o_totalprice     decimal(15, 2) NOT NULL,
+               o_orderdate      DATE NOT NULL,
+               o_orderpriority  VARCHAR(15) NOT NULL,
+               o_clerk          VARCHAR(15) NOT NULL,
+               o_shippriority   int NOT NULL,
+               o_comment        VARCHAR(79) NOT NULL,
+               o_null VARCHAR(1) NULL
+           )ENGINE=OLAP
+           UNIQUE KEY(`o_orderkey`)
+           COMMENT "OLAP"
+           DISTRIBUTED BY HASH(`o_orderkey`) BUCKETS 96
+           PROPERTIES (
+               "replication_num" = "1",
+               "colocate_with" = "lineitem_orders",
+               "enable_unique_key_merge_on_write" = "false"
+           );"""
+           def stream_load_job = { table_name, src_file_name ->
+               streamLoad {
+                   table table_name
+                   set 'column_separator', '|'
+                   file """${getS3Url() + '/regression/tpch/sf100/'}${src_file_name}"""
+
+                   check { result, exception, startTime, endTime ->
+                       if (exception != null) {
+                           throw exception
+                       }
+                       log.info("Stream load result: ${result}".toString())
+                       def json = parseJson(result)
+                       assertEquals("success", json.Status.toLowerCase())
+                       assertEquals(json.NumberTotalRows, json.NumberLoadedRows)
+                       assertTrue(json.NumberLoadedRows > 0 && json.LoadBytes > 0)
+                   }
+
+               }
+               sql "select count(*) from ${table_name}"
+           }
+           for (int i = 1; i <= 10; i++) {
+               stream_load_job(table_name1, "lineitem.tbl.${i}")
+               stream_load_job(table_name2, "orders.tbl.${i}")
+           }
+
+    def mv_name = "single_thread_cancel_mtmv_fail_mtmv"
+
+    for (int num = 0; num < 100; num++) {
+        sql """drop MATERIALIZED VIEW if exists ${mv_name};"""
+        sql """
+        CREATE MATERIALIZED VIEW ${mv_name}
+        BUILD IMMEDIATE REFRESH COMPLETE ON MANUAL
+        DISTRIBUTED BY RANDOM BUCKETS 2
+        PROPERTIES ('replication_num' = '1') AS
+        SELECT 
+            li.l_orderkey,
+            li.l_quantity,
+            li.l_extendedprice,
+            o.o_orderkey,
+            o.o_totalprice,
+            (li.l_extendedprice * (1 - li.l_discount) * (1 + li.l_tax)) + 
+            (SELECT AVG(li2.l_extendedprice * (1 - li2.l_discount) * (1 + li2.l_tax)) 
+             FROM lineitem li2) +
+            (SELECT AVG(o2.o_totalprice) 
+             FROM orders o2),
+            li.l_extendedprice * li.l_tax,
+            CASE 
+                WHEN o.o_orderpriority = '1-URGENT' THEN 1.5
+                WHEN o.o_orderpriority = '2-HIGH' THEN 1.2
+                WHEN o.o_orderpriority = '3-MEDIUM' THEN 1.0
+                WHEN o.o_orderpriority = '4-LOW' THEN 0.8
+                ELSE 0.5
+            END * 
+            (SELECT AVG(o3.o_shippriority) FROM orders o3)
+        FROM 
+            lineitem li
+        JOIN 
+            orders o ON li.l_orderkey = o.o_orderkey
+        WHERE 
+            li.l_quantity > (SELECT AVG(l_quantity) FROM lineitem)
+            AND 
+            o.o_totalprice > (SELECT AVG(o_totalprice) FROM orders)
+        """
+
+        sleep(5 * 1000)
+        def job_name = getJobName(src_database_name, mv_name)
+        def task_id = sql """select * from tasks("type"="mv") where JobName="${job_name}";"""
+        sql """CANCEL MATERIALIZED VIEW TASK ${task_id[0][0]} ON ${mv_name}"""
+
+        sql """refresh MATERIALIZED VIEW ${mv_name} COMPLETE"""
+
+        def task_list = sql """select * from tasks("type"="mv") where JobName="${job_name}" order by StartTime;"""
+        logger.info("task_list:" + task_list)
+        sleep(60 * 1000)
+        task_list = sql """select * from tasks("type"="mv") where JobName="${job_name}" order by StartTime;"""
+        logger.info("task_list:" + task_list)
+
+        assertTrue(task_list.size() == 2)
+        assertTrue(task_list[1][7] == "FAILED" || task_list[1][7] == "SUCCESS")
+        if (task_list[1][7] == "FAIL") {
+            assertTrue(task_list[1][8].indexOf("Not allowed") != -1)
+        }
+    }
+
+
+
+}
